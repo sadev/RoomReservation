d47a44005d09b19cd49991568311f4b6b2ae762b
AdnanZi <adnan.zildzic@gmail.com>
2014-02-20 12:50:11 +0100

    Added project structure

---
+++ RoomReservation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RoomReservation.MvcApplication" Language="C#" %>
---
+++ RoomReservation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RoomReservation.Startup))]
+namespace RoomReservation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
