e38ff0d55a35d8556f67f7c00ef233f8786ea63b
AdnanZi <adnan.zildzic@gmail.com>
2014-02-20 15:19:00 +0100

    Modified home page logic; Cleaned project

--- RoomReservation/App_Start/Startup.Auth.cs
+++ RoomReservation/App_Start/Startup.Auth.cs
@@ -32,7 +32,7 @@ namespace RoomReservation
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication();
+            app.UseGoogleAuthentication();
         }
     }
 }
