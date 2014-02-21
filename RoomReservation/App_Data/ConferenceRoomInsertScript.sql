b23c3d669685e213f2fa652ba5856f12472822eb
Tarik Mesetovic <tarikmes@maestralsolutions.com>
2014-02-21 15:55:40 +0100

    Added db script and changed connection string

---
+++ RoomReservation/App_Data/ConferenceRoomInsertScript.sql
@@ -0,0 +1,11 @@
+
+
+
+--Inserts Conference rooms. If new room  
+begin transaction
+
+INSERT INTO Rooms ( Title ) VALUES ('Conference room 1')
+INSERT INTO Rooms ( Title ) VALUES ('Conference room 2')
+INSERT INTO Rooms ( Title ) VALUES ('Conference room 3')
+
+commit transaction
