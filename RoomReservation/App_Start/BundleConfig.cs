7cbbbd51642d724a68b93c2ec6c6d90aa1a48b62
Tarik Mesetovic <tarikmes@maestralsolutions.com>
2014-02-24 16:15:38 +0100

    Addes tiles nad meto-ui

--- RoomReservation/App_Start/BundleConfig.cs
+++ RoomReservation/App_Start/BundleConfig.cs
@@ -29,11 +29,16 @@ namespace RoomReservation
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/metro.min.js",
                       "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/metro").Include(
+                      "~/Scripts/metro.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
+                      "~/Content/metro-bootstrap.css",
                       "~/Content/fullcalendar.css"));
         }
     }
